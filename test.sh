411c3ed2f8c9d568b6b3535344559a7d9b50609e
Pablo Arango <pabloaran17@gmail.com>
2015-01-19 22:16:26 -0500

    testing script and tests

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for i in {0..5}; do
+  test_out=$(cat tests/test_in$i.txt | ruby ./simple_database.rb)
+  correct_out=$(cat tests/correct_out$i.txt)
+
+  if [ -z "$correct_out" ]; then
+    echo "Test $i output NOT FOUND"
+  else
+    if [ "$test_out" = "$correct_out" ]; then
+      echo "Test $i.........PASSED"
+    else
+      echo "Test $i.........FAILED"
+      echo "Test Output:"
+      echo $test_out
+      echo "Correct Output:"
+      echo $correct_out
+    fi
+  fi
+done
